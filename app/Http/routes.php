dd9c543e6c336ab90fef9d604a6e0def97600818
tomasz <tomasznicieja@gmail.com>
2016-08-28 22:03:01 +0200

    setting new routes for tasks module

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,46 +21,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/**
- * Show Task Dashboard
- */
-Route::get('/', function () {
-    $tasks = Task::orderBy('created_at', 'asc')->get();
+Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
+Route::delete('/tasks/{task}', 'TaskController@destroy');
 
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
-
-/**
- * Add New Task
- */
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
-/**
- * Delete Task
- */
-Route::delete('/task/{task}', function (Task $task) {
-    $task->delete();
-
-    return redirect('/');
-});
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
